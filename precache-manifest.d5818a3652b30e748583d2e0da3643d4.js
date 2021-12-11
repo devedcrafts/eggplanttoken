ab416fe7515be01ced32e60112cdff5a76dff305
“devedcrafter” <devedcrafts@protonmail.com>
2021-12-11 01:50:24 +0100

    Updates

---
+++ precache-manifest.d5818a3652b30e748583d2e0da3643d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14abe6c04aaa72b49f4a688fedc1f394",
+    "url": "/eggplanttoken/index.html"
+  },
+  {
+    "revision": "9cdfc4ca5b8f8e68e6ac",
+    "url": "/eggplanttoken/static/css/main.d731e969.chunk.css"
+  },
+  {
+    "revision": "b0e50e2012535e906d18",
+    "url": "/eggplanttoken/static/js/2.e6ea36a7.chunk.js"
+  },
+  {
+    "revision": "9cdfc4ca5b8f8e68e6ac",
+    "url": "/eggplanttoken/static/js/main.38af5db9.chunk.js"
+  },
+  {
+    "revision": "d082a3b862105c89dd36",
+    "url": "/eggplanttoken/static/js/runtime-main.ed34fb8f.js"
+  },
+  {
+    "revision": "4b41a3475132bd861b30a878e30aa56a",
+    "url": "/eggplanttoken/static/media/Document.4b41a347.pdf"
+  },
+  {
+    "revision": "50c1490d3285a3912d2158585d466cf7",
+    "url": "/eggplanttoken/static/media/eggplantlogo.50c1490d.svg"
+  },
+  {
+    "revision": "8f85acebd44da6f80da853da62c6c26c",
+    "url": "/eggplanttoken/static/media/rainingeggplantsplain.8f85aceb.svg"
+  }
+]);
